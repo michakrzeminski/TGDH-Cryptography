04cafb14c4444151c29c666c00b754a98e3f1367
michakrzeminski <micha.krzeminski@gmail.com>
2017-04-20 22:27:14 +0200

    Client and Server + documentation

---
+++ standardScenario.bat
@@ -0,0 +1,9 @@
+start cmd /k Call startServer.bat 
+TIMEOUT /T 1
+start cmd /k Call startClient.bat
+TIMEOUT /T 1
+start cmd /k Call startClient.bat
+TIMEOUT /T 1
+start cmd /k Call startClient.bat
+TIMEOUT /T 1
+start cmd /k Call startClient.bat
